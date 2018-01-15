0a70ab7c151a8a2418f21e062f07727d306eb54c
kelr <kruan6818@gmail.com>
2018-01-14 21:29:51 -0500

    Skeletons

--- vector/vector.cpp
+++ vector/vector.cpp
@@ -1,2 +1,10 @@
 #include "vector.h"
 
+template <typename vector>
+
+
+void kel_vector<vector>::push_back(const vector& element)
+{
+	array[size] = element
+	size++;
+}
--- vector/vector.h
+++ vector/vector.h
@@ -2,6 +2,41 @@
 #define _KEL_VECTOR_H_
 
 #include "stdio.h"
+/*
+So, this vector has to satisfy some requirements. Because rasins.
+
+vector()        O(1)
+vector(n, x)    O(n)
+size()          O(1)
+v[ i ]          O(1)
+push_back(x)    O(1)
+pop_back        O(1)
+insert          O(size())
+erase           O(size())
+front, back     O(1)
+*/
+
+template <class T> class kel_vector
+{
+public:
+	// Indexing operator
+	T& kel_vector<T>::operator [ ] (unsigned int i)
+	{
+		// Dynamic range checking
+		assert ((i >= 0) && (index < size));
+		return array[i];
+	}
+
+
+
+private:
+	// How large the array is
+	int capacity;
+	// How many elements are in the array
+	int size;
+	// Pointer to the array
+	T *array;
+};
 
 
 
