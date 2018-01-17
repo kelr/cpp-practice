1ae008e47e8f89c47720bcb733b7c38328ae09c3
kelr <kruan6818@gmail.com>
2018-01-16 21:41:34 -0500

    My cpp is pretty rusty

--- array_search/search.cpp
+++ array_search/search.cpp
@@ -1,11 +1,13 @@
+#include <cassert>
+#include <cmath> //floor
+#include <iostream>
 #include <search.h>
 
-int sequential_search(int target, const int target_array[])
+int sequential_search(int target, const int *target_array, std::size_t size)
 {   
     //Oh boy I sure hope there's things in the array
     int ret_val = -1;
-    std::size_t size = sizeof(target_array) / sizeof(target_array[0]);
-    for (std::size_t i = 0; i < size, i++)
+    for (std::size_t i = 0; i < size; i++)
     {
         if (target == target_array[i])
         {
@@ -15,11 +17,9 @@ int sequential_search(int target, const int target_array[])
     return ret_val;
 }
 
-
-int binary_search(int target, const int sorted_array[])
+int binary_search(int target, const int *sorted_array, std::size_t size)
 {
     //Oh boy I sure hope this array is sorted
-    std::size_t size = sizeof(sorted_array) / sizeof(sorted_array[0]);
     std::size_t low = 0;
     std::size_t mid = 0;
     std::size_t high = size;
@@ -38,12 +38,37 @@ int binary_search(int target, const int sorted_array[])
         {
             low = mid + 1;
             mid = static_cast<std::size_t>(floor((static_cast<float>(high) + static_cast<float>(low)) / 2.0));
+            std::cout << mid << std::endl;
         }
         else
         {
-            high = mid - 1
+            high = mid - 1;
             mid = static_cast<std::size_t>(floor((static_cast<float>(high) + static_cast<float>(low)) / 2.0));
         }
     }
     return found_target;
+}
+
+int main(void)
+{
+    const int test_array[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    // Get the size of the C style array
+    std::size_t size = sizeof(test_array)/sizeof(test_array[0]);
+
+    int target = 3;
+
+    int seq_result = sequential_search(target, test_array, size);
+    int binary_result = binary_search(target, test_array, size);
+    //std::cout << seq_result << std::endl;
+    //std::cout << binary_result << std::endl;
+    assert(seq_result == 2);
+    assert(binary_result == 2);
+
+    seq_result = sequential_search(8, test_array, size);
+    binary_result = binary_search(8, test_array, size);
+    //std::cout << seq_result << std::endl;
+    //std::cout << binary_result << std::endl;
+    assert(seq_result == 7);
+    assert(binary_result == 7);
+    return 0;
 }
--- array_search/search.h
+++ array_search/search.h
@@ -3,7 +3,8 @@
 
 #include <cstddef> //size_t
 
-int sequential_search(int target, const int target_array[]);
-int binary_search(int target, const int sorted_array[]);
 
-#endif
+int sequential_search(int target, const int target_array[], std::size_t size);
+int binary_search(int target, const int sorted_array[], std::size_t size);
+
+#endif //_KEL_SEARCH_H_
